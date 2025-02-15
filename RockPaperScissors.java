8f7e4c5e15b558d7adbb897e56e12b6cfc6a03e8
Ishani-1802 <ishani.lohar18@gmail.com>
2025-02-15 17:08:30 +0530

    Update RockPaperScissors.java

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -5,40 +5,41 @@ public class RockPaperScissors{
     public static void main(String[] args){
         String [] choices = {"Rock", "Paper", "Scissors"};
         String computerChoice = choices[new Random().nextInt(3)];
-
+        String playerMove;
         Scanner scanner =new Scanner(System.in);
-        String playerMove = scanner.nextLine();
+        
         while(true){
             System.out.println("Hey! Please enter your move(Rock , Paper or Scissors):");
+            playerMove = scanner.nextLine();
             
-            if(playerMove.equals("Rock")||playerMove.equals("Paper")||playerMove.equals("Scissors")){
+            if(playerMove.equalsIgnoreCase("Rock")||playerMove.equalsIgnoreCase("Paper")||playerMove.equalsIgnoreCase("Scissors")){
                 break;
             }
             System.out.println("Invalid move. Please enter Rock, Paper or Scissors");
         }
 
         System.out.println("Computer's move: "+computerChoice);
-        if(playerMove.equals(computerChoice)){
+        if(playerMove.equalsIgnoreCase(computerChoice)){
             System.out.println("It's a tie!");
         }
-        else if(playerMove.equals("Rocks")){
-            if(computerChoice.equals("Scissors")){
+        else if(playerMove.equalsIgnoreCase("Rock")){
+            if(computerChoice.equalsIgnoreCase("Scissors")){
                 System.out.println("You win!");
             }
             else{
                 System.out.println("You lose!");
             }
         }
-        else if(playerMove.equals("Paper")){
-            if(computerChoice.equals("Scissors")){
+        else if(playerMove.equalsIgnoreCase("Paper")){
+            if(computerChoice.equalsIgnoreCase("Scissors")){
                 System.out.println("You lose!");
             }
             else{
                 System.out.println("You lose!");
             }
         }
-        else if(playerMove.equals("Scissors")){
-            if(computerChoice.equals("Paper")){
+        else if(playerMove.equalsIgnoreCase("Scissors")){
+            if(computerChoice.equalsIgnoreCase("Paper")){
                 System.out.println("You win!");
             }
             else{
